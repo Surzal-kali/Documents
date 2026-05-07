4d9f9c24d3f401ec3d10bc3a0b1cd50ec316ffeb
surzvtr5h <vgreenwald@student.cnm.edu>
2026-05-07 13:37:55 -0600

    Rename random_string to string_burp and modify logic
    Renamed method from random_string to string_burp and updated its implementation to include newline characters.

--- sleepbaby.py
+++ sleepbaby.py
@@ -3,16 +3,19 @@ import random
 import string
 import subprocess
 import os
-from fileshuttle import FileShuttle as fs
+import shutil
+
 #hello from my phone
 class SleepBaby:
     def __init__(self, sleep_time=5):
         self.sleep_time = sleep_time
         self.fs = fs()
 
-    def random_string(self, length=10):
+    def string_burp(self, length=10):
         letters = string.ascii_letters
-        return ''.join(random.choice(letters) for i in range(length))
+        raw = ''.join(random.choice(letters) for i in range(length))
+        realraw = "\n" + raw + "\n"
+        return 
 
 #things to look into to finish this:
 
