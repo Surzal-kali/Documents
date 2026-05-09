35549e814f225e7b6b6e9ddfd896f48f93c7e204
Surzal-kali <vgreenwald@student.cnm.edu>
2026-05-08 21:02:36 -0600

    StackOverflow Encryption Example

--- SurzsEnviro/sleepbaby.py
+++ SurzsEnviro/sleepbaby.py
@@ -29,6 +29,29 @@ class SleepBaby:
         nonce = iv.encode() 
         plaintext = input.encode()
         aesgcm = AESGCM(key)
-        
+        ciphertext=aesgcm.encrypt(nonce, plaintext, None)
         ciphertext_str = base64.b64encode(ciphertext).decode()
         return ciphertext_str
+        
+   def decrypt_with_aes(input:str, enc_key: str, iv: str)
+       key = enc_key.encode()
+       nonce = iv.encode()
+       ciphertext = base64.b64decode(input
+       aesgcm = AESGCM(key)     
+       decrypted = aesgcm.decrypt(nonce, ciphertext, None)
+       return decrypted.decode()
+       
+  def generate_iv_string(length=6
+      chars = string.ascii_letters + string.digits + "#$()*+,-.:;<=>?@[]_"
+      return ''.join(random.choices(chars, k=length))     
+      
+ enc_key = "1Xt5YfM4ZNuFdwp3OfVkwkhhQLagWKtt"
+ iv = generate_iv_string(12)
+ input = "F OFF"
+ 
+ ciphertext = encrypt_with_aes(input, enc_key, iv)
+ print("Ciphertext:", ciphertext) 
+ 
+ decrypted = decrypt_with_aes(ciphertext, enc_key, iv)
+ print("Decrypted:", decrypted
+   
